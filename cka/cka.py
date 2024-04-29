edb9be9c35d2815244342f36a4f20a79fe811383
DigitalLifeYZQiu <qiuyunzhong2002@gmail.com>
2024-04-29 22:30:17 +0800

    CKA Attempt 1

---
+++ cka/cka.py
@@ -0,0 +1,277 @@
+import inspect
+from typing import Callable, Literal
+from warnings import warn
+
+import matplotlib.pyplot as plt
+import seaborn as sn
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision.models.feature_extraction import create_feature_extractor
+from tqdm import tqdm
+
+from .utils import linear_kernel, rbf_kernel, center_matrix
+
+
+class CKA(nn.Module):
+
+    def __init__(
+        self,
+        first_model: nn.Module,
+        second_model: nn.Module,
+        layers: list[str],
+        second_layers: list[str] = None,
+        first_name: str = None,
+        second_name: str = None,
+        device: str = "cpu",
+        kernel: Literal["linear", "rbf"] = "linear",
+    ) -> None:
+        """
+        Centered Kernel Alignment (CKA) implementation. Given a set of examples, CKA compares the representations of
+        examples passed through the layers that we want to compare.
+        :param first_model: the first model whose layer features we want to compare.
+        :param second_model: the second model whose layer features we want to compare.
+        :param layers: list of layers name under inspection (if no "second_layers" is provided, then the layers will
+            count for the second model too).
+        :param second_layers: list of layers from the second model under inspection (default=None).
+        :param first_name: name of the first model (default=None).
+        :param second_name: name of the second model (default=None).
+        :param device: the device used during the computation (default="cpu").
+        :param kernel: the type of kernel, can be either "linear" or "rbf" (default="linear").
+        """
+        super().__init__()
+
+        # Set up the kernel
+        assert kernel in ["rbf", "linear"], ValueError("The kernel must be either 'linear' or 'rbf'.")
+        self.kernel = kernel
+
+        # Check if no layers were passed
+        assert layers is not None and len(layers) > 0, ValueError(
+            "You can not pass 'None' or an empty list as layers. We suggest using 'get_graph_node_names' from the"
+            "'torchvision' package in order to see which layers can be passed."
+        )
+
+        # Remove potential duplicates
+        layers = sorted(set(layers), key=layers.index)
+
+        # Check if to many layers were passed
+        if len(layers) > 100:
+            warn(
+                f"You passed {len(layers)} distinct layers, which is way too high. Consider passing only those"
+                f"layers whose features you are really interested about."
+            )
+
+        # Work with the second model's layers if passed
+        if second_layers is None or len(second_layers) == 0:
+            second_layers = layers.copy()
+        else:
+            # Remove potential duplicates
+            second_layers = sorted(set(second_layers), key=second_layers.index)
+
+            # Check if too many layers were passed
+            if len(second_layers) > 100:
+                warn(
+                    f"You passed {len(second_layers)} distinct layers for the second model, which is way too high."
+                    f"Consider passing only those layers whose features you are really interested about."
+                )
+
+        # Build the extractors, they work like a normal torch.nn.Module, but their output is a dict containing the
+        # features of each layer under their scope.
+        import pdb
+        pdb.set_trace()
+        
+        self.first_extractor = create_feature_extractor(first_model, layers).to(device)
+        self.second_extractor = create_feature_extractor(second_model, second_layers).to(device)
+
+        # Manage the models names
+        first_name = first_name if first_name is not None else first_model.__repr__().split("(")[0]
+        second_name = second_name if second_name is not None else second_model.__repr__().split("(")[0]
+        if first_name == second_name and first_model is not second_model:
+            warn(f"Both models are called {first_name}, beware that it may cause confusion when analyzing the results.")
+
+        # Set up the models infos
+        self.first_model_infos = {"name": first_name, "layers": layers}
+        self.second_model_infos = {"name": second_name, "layers": second_layers}
+
+        # At last, save the device used during the computation
+        self.device = torch.device(device)
+
+    def compute_cka(
+        self, x: torch.Tensor, y: torch.Tensor, unbiased: bool = False, rbf_threshold: float = 1.0
+    ) -> torch.Tensor:
+        """
+        Compute the Centered Kernel Alignment between two given matrices. Adapted from the one made by Kornblith et al.
+        https://github.com/google-research/google-research/tree/master/representation_similarity.
+        :param x: tensor of shape (n, j).
+        :param y: tensor of shape (n, k).
+        :param unbiased: whether to use the unbiased version of CKA (default=False).
+        :param rbf_threshold: the threshold used by the RBF kernel (default=1.0).
+        :return: a float in [0, 1] that is the CKA value between the two given matrices.
+        """
+        # We need to change the dtype of the tensors for a better precision
+        x = x.type(torch.float64) if not x.dtype == torch.float64 else x
+        y = y.type(torch.float64) if not x.dtype == torch.float64 else y
+
+        # Apply the kernel to the matrices and center them
+        if self.kernel == "linear":
+            f_kernel = linear_kernel
+            kernel_other_args = {}
+        else:
+            f_kernel = rbf_kernel
+            kernel_other_args = {"threshold": rbf_threshold}
+
+        gram_x = f_kernel(x, **kernel_other_args)
+        gram_y = f_kernel(y, **kernel_other_args)
+        centered_gram_x = center_matrix(gram_x, unbiased)
+        centered_gram_y = center_matrix(gram_y, unbiased)
+
+        # This is the final step of computing the Frobenius norm of Y^T * X
+        hsic_xy = centered_gram_x.ravel().dot(centered_gram_y.ravel())
+
+        # Compute the Frobenius norm for both matrix
+        fro_norm_x = torch.linalg.norm(centered_gram_x, ord="fro")
+        fro_norm_y = torch.linalg.norm(centered_gram_y, ord="fro")
+
+        # Finally, compute CKA
+        cka = hsic_xy / (fro_norm_x * fro_norm_y)
+        return cka
+
+    def forward(
+        self,
+        dataloader: DataLoader,
+        unbiased: bool = False,
+        rbf_threshold: float = 1.0,
+        f_extract: Callable[..., dict[str, torch.Tensor]] = None,
+    ) -> torch.Tensor:
+        """
+        Process inputs and computes the CKA matrix.
+        :param dataloader: dataloader that will be used during the computation.
+        :param unbiased: whether to use the unbiased version of CKA (default=False).
+        :param rbf_threshold: the RBF threshold (default=1.0).
+        :param f_extract: the function to apply on the dataloader, this function should take any number and type of
+            inputs and return a dict. If no function is passed, then some checks will be applied for finding the actual
+            type of the batch (default=None).
+        :return: the CKA value.
+        """
+        self.first_extractor.eval()
+        self.second_extractor.eval()
+
+        with torch.no_grad():
+            n = len(self.first_model_infos["layers"])
+            m = len(self.second_model_infos["layers"])
+            cka = torch.zeros(n, m, device=self.device)
+
+            # Iterate through the dataset
+            num_batches = len(dataloader)
+            for batch in tqdm(dataloader, desc="| Computing CKA |", total=num_batches):
+                if f_extract is not None:
+                    # Apply the provided function and put everything on the device
+                    batch: dict[str, torch.Tensor] = f_extract(batch)
+                    batch = {f"{name}": batch_input.to(self.device) for name, batch_input in batch.items()}
+                elif isinstance(batch, (list, tuple)):
+                    args_list = inspect.getfullargspec(self.first_extractor.forward).args[1:]  # skip "self" argument
+                    batch = {f"{args_list[i]}": batch_input.to(self.device) for i, batch_input in enumerate(batch)}
+                else:
+                    raise ValueError(
+                        f"Type {type(batch)} is not supported for the CKA computation. We suggest building a custom"
+                        f"'Dataset' class such that the '__get_item__' method returns a dict[str, torch.Tensor]."
+                    )
+
+                # Do a forward pass for both models
+                first_outputs: dict[str, torch.Tensor] = self.first_extractor(**batch)
+                second_outputs: dict[str, torch.Tensor] = self.second_extractor(**batch)
+                for i, (first_out_name, first_out) in enumerate(first_outputs.items()):
+                    x = first_out.view(-1, first_out.shape[-1])
+                    for j, (second_out_name, second_out) in enumerate(second_outputs.items()):
+                        y = second_out.view(-1, second_out.shape[-1])
+                        cka[i, j] += self.compute_cka(x, y, unbiased, rbf_threshold) / num_batches
+
+        # One last check
+        assert not torch.isnan(cka).any(), "CKA computation resulted in NANs"
+
+        return cka
+
+    def plot_cka(
+        self,
+        cka_matrix: torch.Tensor,
+        save_path: str = None,
+        title: str = None,
+        show_ticks_labels: bool = False,
+        short_tick_labels_splits: int | None = None,
+        use_tight_layout: bool = True,
+        show_annotations: bool = True,
+        show_img: bool = True,
+        **kwargs,
+    ) -> None:
+        """
+        Plot the CKA matrix obtained calling this class' forward() method.
+        :param cka_matrix: the CKA matrix.
+        :param save_path: the path where to save the plot, if None then the plot will not be saved (default=None).
+        :param title: the plot title, if None then no title will be used (default=None).
+        :param show_ticks_labels: whether to show the tick labels (default=False).
+        :param short_tick_labels_splits: only works when show_tick_labels is True. If it is not None, the tick labels
+            will be shortened to the defined sublayer starting from the deepest level. E.g.: if the layer name is
+            'encoder.ff.linear' and this parameter is set to 1, then only 'linear' will be printed on the heatmap
+            (default=None).
+        :param use_tight_layout: whether to use a tight layout in order not to cut any label in the plot (default=True).
+        :param show_annotations: whether to show the annotations on the heatmap (default=True).
+        :param show_img: whether to show the plot (default=True).
+        :return:
+        """
+        # Build the heatmap
+        vmin: float | None = kwargs.get("vmin", None)
+        vmax: float | None = kwargs.get("vmax", None)
+        if (vmin is not None) ^ (vmax is not None):
+            raise ValueError("'vmin' and 'vmax' must be defined together or both equal to None.")
+
+        cmap = kwargs.get("cmap", "magma")
+        vmin = min(vmin, torch.min(cka_matrix).item()) if vmin is not None else vmin
+        vmax = max(vmax, torch.max(cka_matrix).item()) if vmax is not None else vmax
+        ax = sn.heatmap(cka_matrix.cpu(), vmin=vmin, vmax=vmax, annot=show_annotations, cmap=cmap)
+        ax.invert_yaxis()
+        ax.set_xlabel(f"{self.second_model_infos['name']} layers", fontsize=12)
+        ax.set_ylabel(f"{self.first_model_infos['name']} layers", fontsize=12)
+
+        # Deal with tick labels
+        if show_ticks_labels:
+            if short_tick_labels_splits is None:
+                ax.set_xticklabels(self.second_model_infos["name"])
+                ax.set_yticklabels(self.first_model_infos["name"])
+            else:
+                ax.set_xticklabels(
+                    [
+                        "-".join(module.split(".")[-short_tick_labels_splits:])
+                        for module in self.second_model_infos["layers"]
+                    ]
+                )
+                ax.set_yticklabels(
+                    [
+                        "-".join(module.split(".")[-short_tick_labels_splits:])
+                        for module in self.first_model_infos["layers"]
+                    ]
+                )
+
+            plt.xticks(rotation=90)
+            plt.yticks(rotation=0)
+
+        # Put the title if passed
+        chart_title = title
+        if title is not None:
+            ax.set_title(f"{title}", fontsize=14)
+        else:
+            chart_title = f"{self.first_model_infos['name']} vs {self.second_model_infos['name']}"
+            ax.set_title(chart_title, fontsize=14)
+
+        # Set the layout to tight if the corresponding parameter is True
+        if use_tight_layout:
+            plt.tight_layout()
+
+        # Save the plot to the specified path if defined
+        if save_path is not None:
+            chart_title = chart_title.replace("/", "-")
+            path_rel = f"{save_path}/{chart_title}.png"
+            plt.savefig(path_rel, dpi=400, bbox_inches="tight")
+
+        # Show the image if the user chooses to do so
+        if show_img:
+            plt.show()
---
+++ ckaCalculator/cka.py
@@ -0,0 +1,292 @@
+"""
+Tool to compute Centered Kernel Alignment (CKA) in PyTorch w/ GPU (single or multi).
+
+Repo: https://github.com/numpee/CKA.pytorch
+Author: Dongwan Kim (Github: Numpee)
+Year: 2022
+"""
+
+from __future__ import annotations
+
+from typing import Tuple, Optional, Callable, Type, Union, TYPE_CHECKING, List
+
+import torch
+import torch.nn as nn
+from tqdm.autonotebook import tqdm
+
+# from hook_manager import HookManager, _HOOK_LAYER_TYPES
+from ckaCalculator.hook_manager import HookManager, _HOOK_LAYER_TYPES
+# from metrics import AccumTensor
+from ckaCalculator.metrics import AccumTensor
+
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+if TYPE_CHECKING:
+    from torch.utils.data import DataLoader
+
+
+class CKACalculator:
+    def __init__(self, args, device, model1: nn.Module, model2: nn.Module, dataloader: DataLoader,
+                 hook_fn: Optional[Union[str, Callable]] = None,
+                 hook_layer_types: Tuple[Type[nn.Module], ...] = _HOOK_LAYER_TYPES, num_epochs: int = 10,
+                 group_size: int = 512, epsilon: float = 1e-4, is_main_process: bool = True) -> None:
+        """
+        Class to extract intermediate features and calculate CKA Matrix.
+        :param model1: model to evaluate. __call__ function should be implemented if NOT instance of `nn.Module`.
+        :param model2: second model to evaluate. __call__ function should be implemented if NOT instance of `nn.Module`.
+        :param dataloader: Torch DataLoader for dataloading. Assumes first return value contains input images.
+        :param hook_fn: Optional - Hook function or hook name string for the HookManager. Options: [flatten, avgpool]. Default: flatten
+        :param hook_layer_types: Types of layers (modules) to add hooks to.
+        :param num_epochs: Number of epochs for cka_batch. Default: 10
+        :param group_size: group_size for GPU acceleration. Default: 512
+        :param epsilon: Small multiplicative value for HSIC. Default: 1e-4
+        :param is_main_process: is current instance main process. Default: True
+        """
+        self.args = args
+        self.device = device
+        self.model1 = model1
+        self.model2 = model2
+        self.dataloader = dataloader
+        self.num_epochs = num_epochs
+        self.group_size = group_size
+        self.epsilon = epsilon
+        self.is_main_process = is_main_process
+
+        self.model1.eval()
+        self.model2.eval()
+        self.hook_manager1 = HookManager(self.model1, hook_fn, hook_layer_types, calculate_gram=True)
+        self.hook_manager2 = HookManager(self.model2, hook_fn, hook_layer_types, calculate_gram=True)
+        self.module_names_X = None
+        self.module_names_Y = None
+        self.num_layers_X = None
+        self.num_layers_Y = None
+        self.num_elements = None
+
+        # Metrics to track
+        self.cka_matrix = None
+        self.hsic_matrix = None
+        self.self_hsic_x = None
+        self.self_hsic_y = None
+
+    @torch.no_grad()
+    def calculate_cka_matrix(self) -> torch.Tensor:
+        curr_hsic_matrix = None
+        curr_self_hsic_x = None
+        curr_self_hsic_y = None
+        for epoch in range(self.num_epochs):
+            loader = tqdm(self.dataloader, desc=f"Epoch {epoch}", disable=not self.is_main_process)
+            for it, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(loader):
+                batch_x = batch_x.float().to(self.device)
+                batch_y = batch_y.float().to(self.device)
+
+                batch_x_mark = batch_x_mark.float().to(self.device)
+                batch_y_mark = batch_y_mark.float().to(self.device)
+
+                # decoder input
+                dec_inp = torch.zeros_like(batch_y[:, -self.args.pred_len:, :]).float()
+                dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+
+                self.model1(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                self.model2(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                all_layer_X, all_layer_Y = self.extract_layer_list_from_hook_manager()
+
+                # Initialize values on first loop
+                if self.num_layers_X is None:
+                    curr_hsic_matrix, curr_self_hsic_x, curr_self_hsic_y = self._init_values(all_layer_X, all_layer_Y)
+
+                # Get self HSIC values --> HSIC(K, K), HSIC(L, L)
+                self._calculate_self_hsic(all_layer_X, all_layer_Y, curr_self_hsic_x, curr_self_hsic_y)
+
+                # Get cross HSIC values --> HSIC(K, L)
+                self._calculate_cross_hsic(all_layer_X, all_layer_Y, curr_hsic_matrix)
+
+                self.hook_manager1.clear_features()
+                self.hook_manager2.clear_features()
+                curr_hsic_matrix.fill_(0)
+                curr_self_hsic_x.fill_(0)
+                curr_self_hsic_y.fill_(0)
+
+        # Update values across GPUs
+        hsic_matrix = self.hsic_matrix.compute()
+        hsic_x = self.self_hsic_x.compute()
+        hsic_y = self.self_hsic_y.compute()
+        self.cka_matrix = hsic_matrix.reshape(self.num_layers_Y, self.num_layers_X) / torch.sqrt(hsic_x * hsic_y)
+        # print(self.cka_matrix.diagonal())
+        # self.cka_matrix = self.cka_matrix.flip(0)
+        return self.cka_matrix
+
+    def extract_layer_list_from_hook_manager(self) -> Tuple[List, List]:
+        all_layer_X, all_layer_Y = self.hook_manager1.get_features(), self.hook_manager2.get_features()
+        return all_layer_X, all_layer_Y
+
+    def hsic1(self, K: torch.Tensor, L: torch.Tensor) -> torch.Tensor:
+        '''
+        Batched version of HSIC.
+        :param K: Size = (B, N, N) where N is the number of examples and B is the group/batch size
+        :param L: Size = (B, N, N) where N is the number of examples and B is the group/batch size
+        :return: HSIC tensor, Size = (B)
+        '''
+        assert K.size() == L.size()
+        assert K.dim() == 3
+        K = K.clone()
+        L = L.clone()
+        n = K.size(1)
+
+        # K, L --> K~, L~ by setting diagonals to zero
+        K.diagonal(dim1=-1, dim2=-2).fill_(0)
+        L.diagonal(dim1=-1, dim2=-2).fill_(0)
+
+        KL = torch.bmm(K, L)
+        trace_KL = KL.diagonal(dim1=-1, dim2=-2).sum(-1).unsqueeze(-1).unsqueeze(-1)
+        middle_term = K.sum((-1, -2), keepdim=True) * L.sum((-1, -2), keepdim=True)
+        middle_term /= (n - 1) * (n - 2)
+        right_term = KL.sum((-1, -2), keepdim=True)
+        right_term *= 2 / (n - 2)
+        main_term = trace_KL + middle_term - right_term
+        hsic = main_term / (n ** 2 - 3 * n)
+        return hsic.squeeze(-1).squeeze(-1)
+
+    def reset(self) -> None:
+        # Set values to none, clear feature and hooks
+        self.cka_matrix = None
+        self.hsic_matrix = None
+        self.self_hsic_x = None
+        self.self_hsic_y = None
+        self.hook_manager1.clear_all()
+        self.hook_manager2.clear_all()
+
+    def _init_values(self, all_layer_X, all_layer_Y):
+        self.num_layers_X = len(all_layer_X)
+        self.num_layers_Y = len(all_layer_Y)
+        self.module_names_X = self.hook_manager1.get_module_names()
+        self.module_names_Y = self.hook_manager2.get_module_names()
+        self.num_elements = self.num_layers_Y * self.num_layers_X
+        curr_hsic_matrix = torch.zeros(self.num_elements).cuda()
+        curr_self_hsic_x = torch.zeros(1, self.num_layers_X).cuda()
+        curr_self_hsic_y = torch.zeros(self.num_layers_Y, 1).cuda()
+        self.hsic_matrix = AccumTensor(torch.zeros_like(curr_hsic_matrix)).cuda()
+        self.self_hsic_x = AccumTensor(torch.zeros_like(curr_self_hsic_x)).cuda()
+        self.self_hsic_y = AccumTensor(torch.zeros_like(curr_self_hsic_y)).cuda()
+        return curr_hsic_matrix, curr_self_hsic_x, curr_self_hsic_y
+
+    def _calculate_self_hsic(self, all_layer_X, all_layer_Y, curr_self_hsic_x, curr_self_hsic_y):
+        for start_idx in range(0, self.num_layers_X, self.group_size):
+            end_idx = min(start_idx + self.group_size, self.num_layers_X)
+            K = torch.stack([all_layer_X[i] for i in range(start_idx, end_idx)], dim=0)
+            curr_self_hsic_x[0, start_idx:end_idx] += self.hsic1(K, K) * self.epsilon
+        for start_idx in range(0, self.num_layers_Y, self.group_size):
+            end_idx = min(start_idx + self.group_size, self.num_layers_Y)
+            L = torch.stack([all_layer_Y[i] for i in range(start_idx, end_idx)], dim=0)
+            curr_self_hsic_y[start_idx:end_idx, 0] += self.hsic1(L, L) * self.epsilon
+
+        self.self_hsic_x.update(curr_self_hsic_x)
+        self.self_hsic_y.update(curr_self_hsic_y)
+
+    def _calculate_cross_hsic(self, all_layer_X, all_layer_Y, curr_hsic_matrix):
+        for start_idx in range(0, self.num_elements, self.group_size):
+            end_idx = min(start_idx + self.group_size, self.num_elements)
+            K = torch.stack([all_layer_X[i % self.num_layers_X] for i in range(start_idx, end_idx)], dim=0)
+            L = torch.stack([all_layer_Y[j // self.num_layers_X] for j in range(start_idx, end_idx)], dim=0)
+            curr_hsic_matrix[start_idx:end_idx] += self.hsic1(K, L) * self.epsilon
+        self.hsic_matrix.update(curr_hsic_matrix)
+
+    def plot_cka(
+        self,
+        cka_matrix: torch.Tensor,
+        save_path: str = None,
+        title: str = None,
+        show_ticks_labels: bool = False,
+        short_tick_labels_splits: int | None = None,
+        use_tight_layout: bool = True,
+        show_annotations: bool = True,
+        show_img: bool = True,
+        **kwargs,
+    ) -> None:
+        """
+        Plot the CKA matrix obtained calling this class' forward() method.
+        :param cka_matrix: the CKA matrix.
+        :param save_path: the path where to save the plot, if None then the plot will not be saved (default=None).
+        :param title: the plot title, if None then no title will be used (default=None).
+        :param show_ticks_labels: whether to show the tick labels (default=False).
+        :param short_tick_labels_splits: only works when show_tick_labels is True. If it is not None, the tick labels
+            will be shortened to the defined sublayer starting from the deepest level. E.g.: if the layer name is
+            'encoder.ff.linear' and this parameter is set to 1, then only 'linear' will be printed on the heatmap
+            (default=None).
+        :param use_tight_layout: whether to use a tight layout in order not to cut any label in the plot (default=True).
+        :param show_annotations: whether to show the annotations on the heatmap (default=True).
+        :param show_img: whether to show the plot (default=True).
+        :return:
+        """
+        # Build the heatmap
+        vmin: float | None = kwargs.get("vmin", None)
+        vmax: float | None = kwargs.get("vmax", None)
+        if (vmin is not None) ^ (vmax is not None):
+            raise ValueError("'vmin' and 'vmax' must be defined together or both equal to None.")
+
+        cmap = kwargs.get("cmap", "magma")
+        vmin = min(vmin, torch.min(cka_matrix).item()) if vmin is not None else vmin
+        vmax = max(vmax, torch.max(cka_matrix).item()) if vmax is not None else vmax
+        ax = sn.heatmap(cka_matrix.cpu(), vmin=vmin, vmax=vmax, annot=show_annotations, cmap=cmap)
+        ax.invert_yaxis()
+        # import pdb
+        # pdb.set_trace()
+
+        # ax.set_xlabel(f"{self.module_names_Y} layers", fontsize=12)
+        # ax.set_ylabel(f"{self.module_names_X} layers", fontsize=12)
+        ax.set_xlabel("Model 2 layers", fontsize=12)
+        ax.set_ylabel("Model 1 layers", fontsize=12)
+
+        # Deal with tick labels
+        if show_ticks_labels:
+            if short_tick_labels_splits is None:
+                ax.set_xticklabels(self.module_names_Y)
+                ax.set_yticklabels(self.module_names_X)
+            else:
+                ax.set_xticklabels(
+                    [
+                        "-".join(module.split(".")[-short_tick_labels_splits:])
+                        for module in self.module_names_Y
+                    ]
+                )
+                ax.set_yticklabels(
+                    [
+                        "-".join(module.split(".")[-short_tick_labels_splits:])
+                        for module in self.module_names_X
+                    ]
+                )
+
+            plt.xticks(rotation=90)
+            plt.yticks(rotation=0)
+
+        # if show_ticks_labels:
+        #     ax.set_xticklabels(self.module_names_Y)
+        #     ax.set_yticklabels(self.module_names_X)
+        #     plt.xticks(rotation=90)
+        #     plt.yticks(rotation=0)
+
+        # Put the title if passed
+        chart_title = title
+        if title is not None:
+            ax.set_title(f"{title}", fontsize=14)
+        else:
+            chart_title = f"{self.first_model_infos['name']} vs {self.second_model_infos['name']}"
+            ax.set_title(chart_title, fontsize=14)
+
+        # Set the layout to tight if the corresponding parameter is True
+        if use_tight_layout:
+            plt.tight_layout()
+
+        # Save the plot to the specified path if defined
+        if save_path is not None:
+            chart_title = chart_title.replace("/", "-")
+            path_rel = f"{save_path}/{chart_title}.png"
+            plt.savefig(path_rel, dpi=400, bbox_inches="tight")
+
+        # Show the image if the user chooses to do so
+        if show_img:
+            plt.show()
+
+
+def gram(x: torch.Tensor) -> torch.Tensor:
+    return x.matmul(x.t())
